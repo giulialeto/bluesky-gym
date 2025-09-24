a50de8e344e896f4a6df5a8cb19add90c816b874
Giulia Leto <giuliam.leto@gmail.com>
2025-09-24 10:04:40 +0200

    added a version of main with intermediate checkpoints

---
+++ main_checkpoint.py
@@ -0,0 +1,77 @@
+"""
+This file is an example train and test loop for the different environments.
+Selecting different environments is done through setting the 'env_name' variable.
+
+TODO:
+* add rgb_array rendering for the different environments to allow saving videos
+"""
+
+import gymnasium as gym
+from stable_baselines3 import PPO, SAC, TD3, DDPG
+
+import numpy as np
+
+import bluesky_gym
+import bluesky_gym.envs
+
+from bluesky_gym.utils import logger
+
+bluesky_gym.register_envs()
+
+env_name = 'CentralisedStaticObstacleEnv-v0'
+algorithm = SAC
+
+# Initialize logger
+log_dir = f'./logs/{env_name}/'
+file_name = f'{env_name}_{str(algorithm.__name__)}.csv'
+csv_logger_callback = logger.CSVLoggerCallback(log_dir, file_name)
+
+TRAIN = True
+EVAL_EPISODES = 10
+
+
+from stable_baselines3.common.callbacks import CheckpointCallback, EvalCallback
+
+
+
+if __name__ == "__main__":
+    env = gym.make(env_name, render_mode=None)
+    # obs, info = env.reset()
+    model = algorithm("MultiInputPolicy", env, verbose=1,learning_rate=3e-4)
+    if TRAIN:
+        checkpoint_callback = CheckpointCallback(
+            save_freq=100_000,  # save every 100k env steps
+            save_path=f"models/{env_name}/{algorithm.__name__}/",
+            name_prefix="ckpt"
+        )
+
+        eval_env = gym.make(env_name)
+        eval_callback = EvalCallback(
+            eval_env,
+            best_model_save_path=f"models/{env_name}/{algorithm.__name__}/best",
+            n_eval_episodes=5,
+            eval_freq=50_000
+        )
+        model.learn(total_timesteps=int(2e6),
+            callback=[csv_logger_callback, checkpoint_callback, eval_callback])
+        # model.learn(total_timesteps=2e6, callback=csv_logger_callback)
+        model.save(f"models/{env_name}/{env_name}_{str(algorithm.__name__)}/model")
+        del model
+    env.close()
+    
+    # Test the trained model
+    model = algorithm.load(f"models/{env_name}/{env_name}_{str(algorithm.__name__)}/model", env=env)
+    env = gym.make(env_name, render_mode="human")
+    for i in range(EVAL_EPISODES):
+
+        done = truncated = False
+        obs, info = env.reset()
+        tot_rew = 0
+        while not (done or truncated):
+            # action = np.array(np.random.randint(-100,100,size=(2))/100)
+            # action = np.array([0,-1])
+            action, _states = model.predict(obs, deterministic=True)
+            obs, reward, done, truncated, info = env.step(action[()])
+            tot_rew += reward
+        print(tot_rew)
+    env.close()
