ddf8beac163a15f04a8e84eaca7f8eee548e03a2
AK <akorol@AKs-MacBook-Pro.local>
2026-07-10 18:22:55 +0200

    added waypoint reach, and crash statistics to logger

--- bluesky_gym/utils/logger.py
+++ bluesky_gym/utils/logger.py
@@ -1,6 +1,9 @@
 import csv
 import os
+import numpy as np
+from collections import deque
 from stable_baselines3.common.callbacks import BaseCallback
+
 
 class CSVLoggerCallback(BaseCallback):
     def __init__(self, log_dir, file_name='training_log.csv', verbose=0):
@@ -11,6 +14,10 @@ class CSVLoggerCallback(BaseCallback):
         self.headers = ['timesteps', 'episodes']
         self.initialized = False
         self.episode_count = 0
+
+        # ADD
+        self.reached_buffer = deque(maxlen=100)
+        self.crashed_buffer = deque(maxlen=100)
 
     def _on_step(self) -> bool:
         if not self.initialized:
@@ -26,6 +33,13 @@ class CSVLoggerCallback(BaseCallback):
             self.episode_count += 1
             timesteps = self.num_timesteps
             info_dict = self.locals['infos'][0]
+
+            # ADD
+            self.reached_buffer.append(info_dict.get('waypoint_reached', 0))
+            self.crashed_buffer.append(info_dict.get('crashed', 0))
+            self.logger.record('rollout/waypoint_reached_mean', np.mean(self.reached_buffer))
+            self.logger.record('rollout/crashed_mean', np.mean(self.crashed_buffer))
+
             info_values = [info_dict.get(key, None) for key in self.info_keys]
             row = [timesteps, self.episode_count] + list(info_values)
             with open(self.log_file, mode='a', newline='') as f:
